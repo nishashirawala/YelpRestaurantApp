600c46d8830ca6f241f4e61a4d1d280726c86738
Nisha Shirawala <nishashirawala@gmail.com>
2016-05-09 12:59:10 -0400

    Yelp url parsing added

---
+++ app/src/main/java/yelp/YelpAPI.java
@@ -0,0 +1,57 @@
+package yelp;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+
+public class YelpAPI {
+
+    private static String SEARCH_URL = "http://api.yelp.com/v2/search";
+    private static String BUSINESS_URL = "http://api.yelp.com/v2/business";
+    OAuthService service;
+    Token accessToken;
+
+    public static String consumerKey = "IMv6Cs7XEvqy8iT7XcPKcA";
+    public static String consumerSecret = "qoRGwOGWrx34CmV3wDFp3hDaJNk";
+    public static String token = "ZXmuLpTiMlTrhtV90fmRBfuyBjtWT4W_";
+    public static String tokenSecret = "Ci-K4yUAoEPdQpEbWBsmE5ulWJI";
+
+    public static YelpAPI getYelp() {
+        return new YelpAPI(consumerKey, consumerSecret, token, tokenSecret);
+    }
+
+    public YelpAPI(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        this.service = new ServiceBuilder().provider(YelpApiOAuth.class).apiKey(consumerKey).apiSecret(consumerSecret).build();
+        this.accessToken = new Token(token, tokenSecret);
+    }
+
+    public String search(String term, String location) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.yelp.com/v2/search");
+        request.addQuerystringParameter("term", term);
+        request.addQuerystringParameter("location", location);
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+    public String searchForBusinessesByLocation(String term, String location, String limit) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, SEARCH_URL);
+        request.addQuerystringParameter("term", term);
+        request.addQuerystringParameter("location", location);
+        request.addQuerystringParameter("limit", limit);
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+    public String searchBusinessDetail(String businessId) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, BUSINESS_URL + "/" + businessId);
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+}
---
+++ app/src/test/java/com/yelprestaurantapp/yelp/YelpAPITest.java
@@ -0,0 +1,33 @@
+package com.yelprestaurantapp.yelp;
+
+
+import com.yelprestaurantapp.TestConstants;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import yelp.YelpAPI;
+
+import static org.junit.Assert.assertNotNull;
+
+public class YelpAPITest {
+
+    private YelpAPI fixture;
+
+    @Before
+    public void setup() {
+        fixture = new YelpAPI(TestConstants.consumerKey, TestConstants.consumerSecret, TestConstants.token, TestConstants.tokenSecret);
+    }
+
+    @Test
+    public void testSearchForBusinessesByLocation() throws Exception {
+        String response = fixture.search("restaurants", "toronto");
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testGetBusinessDetail() throws Exception {
+        String response = fixture.searchBusinessDetail("corrados-toronto");
+        assertNotNull(response);
+    }
+}
