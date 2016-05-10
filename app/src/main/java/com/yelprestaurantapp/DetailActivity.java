dd39de5197e17fa22a75c34a2de8f86dbe69c94a
Nisha Shirawala <nishashirawala@gmail.com>
2016-05-10 11:40:22 -0400

    Detail page looks fine

--- app/src/main/java/com/yelprestaurantapp/DetailActivity.java
+++ app/src/main/java/com/yelprestaurantapp/DetailActivity.java
@@ -5,21 +5,26 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.yelprestaurantapp.bean.Category;
 import com.yelprestaurantapp.bean.RestaurantDetail;
 import com.yelprestaurantapp.bean.Review;
 import com.yelprestaurantapp.bean.Reviewer;
 import com.yelprestaurantapp.service.RestaurantService;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.cert.X509Certificate;
+import java.util.List;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -36,40 +41,77 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         String businessID = intent.getStringExtra("businessId");
+        if(businessID != null) {
+            new RestaurantDetailServiceAsyncTask(businessID).execute();
+        } else {
+            TextView errorText = (TextView) findViewById(R.id.errorText);
+            errorText.setText("Unable to find restaurant detail. Restaurant Id is null");
+            Log.e("Error", "Business Id can not be null");
+        }
+    }
 
-        new RestaurantDetailServiceAsyncTask(businessID).execute();
+    private class RestaurantDetailServiceAsyncTask extends AsyncTask {
+        String businessId;
+
+        public RestaurantDetailServiceAsyncTask(String businssId) {
+            this.businessId = businssId;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            RestaurantService service = new RestaurantService();
+            RestaurantDetail restaurantDetails = service.getRestaurantDetails(businessId);
+            return restaurantDetails;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            RestaurantDetail restaurantDetail = (RestaurantDetail) o;
+            DetailActivity.this.updateUI(restaurantDetail);
+        }
     }
 
     public void updateUI(RestaurantDetail detail) {
         TextView resNameTextView = (TextView) findViewById(R.id.restaurantName);
         resNameTextView.setText(detail.getName());
-
+        List<Category> categoryList = detail.getCategories();
+        String categoryStr = createCategoryString(categoryList);
+        TextView catTextView = (TextView) findViewById(R.id.categoryTextView);
+        catTextView.setText(categoryStr);
         TextView addressTextView = (TextView) findViewById(R.id.restaurantAddress);
         addressTextView.setText(detail.getDisplayAddress());
-
-        new DownloadHttpsImageTask((ImageView) findViewById(R.id.restaurantImage))
-                .execute(detail.getImageUrl());
-
+        new DownloadHttpsImageTask((ImageView) findViewById(R.id.restaurantImage)).execute(detail.getImageUrl());
         TextView ratingTextView = (TextView) findViewById(R.id.ratingTextView);
         ratingTextView.setText(String.valueOf(detail.getRating()));
-
         Review review = detail.getReview();
         TextView reviewText = (TextView) findViewById(R.id.reviewText);
         reviewText.setText(review.getExcerpt());
-
         TextView userName = (TextView) findViewById(R.id.userName);
         Reviewer reviewer = review.getReviewer();
         userName.setText(reviewer.getUserName());
-
         String userImgUrl = detail.getReview().getReviewer().getUserImageUrl();
         if (userImgUrl.startsWith("http://")) {
-            new DownloadImageTask((ImageView) findViewById(R.id.userImage))
-                    .execute(userImgUrl);
-
+            new DownloadImageTask((ImageView) findViewById(R.id.userImage)).execute(userImgUrl);
         } else {
-            new DownloadHttpsImageTask((ImageView) findViewById(R.id.userImage))
-                    .execute(userImgUrl);
+            new DownloadHttpsImageTask((ImageView) findViewById(R.id.userImage)).execute(userImgUrl);
         }
+    }
+
+    @NonNull
+    private String createCategoryString(List<Category> categoryList) {
+        String categoryStr = "";
+        if(categoryList != null && categoryList.size() >0) {
+            for(int i=0; i<categoryList.size(); i++) {
+                Category c = categoryList.get(i);
+                if(i == categoryList.size()-1) {
+                    categoryStr += c.getName();
+                } else {
+                    categoryStr += c.getName() + " | ";
+                }
+            }
+        }
+        return categoryStr;
     }
 
     private class DownloadHttpsImageTask extends AsyncTask<String, Void, Bitmap> {
@@ -115,28 +157,7 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
-    private class RestaurantDetailServiceAsyncTask extends AsyncTask {
 
-        String businessId;
-
-        public RestaurantDetailServiceAsyncTask(String businssId) {
-            this.businessId = businssId;
-        }
-
-        @Override
-        protected Object doInBackground(Object[] params) {
-            RestaurantService service = new RestaurantService();
-            RestaurantDetail restaurantDetails = service.getRestaurantDetails(businessId);
-            return restaurantDetails;
-        }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-            RestaurantDetail restaurantDetail = (RestaurantDetail) o;
-            DetailActivity.this.updateUI(restaurantDetail);
-        }
-    }
 
     private Bitmap downloadImage(String url) {
         Bitmap bitmap = null;
@@ -148,7 +169,7 @@ public class DetailActivity extends AppCompatActivity {
             stream = getHttpConnection(url);
             bitmap = BitmapFactory.
                     decodeStream(stream, null, bmOptions);
-            if(stream != null) {
+            if (stream != null) {
                 stream.close();
             }
         } catch (IOException e1) {
@@ -167,8 +188,12 @@ public class DetailActivity extends AppCompatActivity {
 
             ctx.init(null, new TrustManager[]{
                     new X509TrustManager() {
-                        public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-                        public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
                         public X509Certificate[] getAcceptedIssuers() {
                             return new X509Certificate[]{};
                         }
