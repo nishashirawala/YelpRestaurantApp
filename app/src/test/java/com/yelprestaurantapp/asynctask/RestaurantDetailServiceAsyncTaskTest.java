5e5d0c2f5792dedd59420669293dc02c65122bf1
Nisha Shirawala <nishashirawala@gmail.com>
2016-06-08 15:15:47 -0400

    Unit test added for detail async task

---
+++ app/src/test/java/com/yelprestaurantapp/asynctask/RestaurantDetailServiceAsyncTaskTest.java
@@ -0,0 +1,40 @@
+package com.yelprestaurantapp.asynctask;
+
+
+import com.yelprestaurantapp.BuildConfig;
+import com.yelprestaurantapp.bean.RestaurantDetail;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class RestaurantDetailServiceAsyncTaskTest implements RestaurantDetailServiceAsyncTask.ResultListener{
+
+    private RestaurantDetailServiceAsyncTask fixture;
+    private RestaurantDetail asyncResult;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        fixture = new RestaurantDetailServiceAsyncTask(this);
+        Assert.assertNotNull(fixture);
+    }
+
+    @Test
+    public void testExecute() {
+        String id = "under-the-table-restaurant-toronto";
+        String[] params = {id};
+        fixture.execute(params);
+        Assert.assertNotNull(asyncResult);
+    }
+    @Override
+    public void handleAsyncResult(RestaurantDetail restaurantDetail) {
+        asyncResult = restaurantDetail;
+    }
+}
