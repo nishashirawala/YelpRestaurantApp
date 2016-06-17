f6201c49e959deb4cb2020c3f177e3f5e7e55f60
Nisha Shirawala <nishashirawala@gmail.com>
2016-06-17 15:54:00 -0400

    Enhancing test

--- app/src/test/java/com/yelprestaurantapp/asynctask/RestaurantDetailServiceAsyncTaskTest.java
+++ app/src/test/java/com/yelprestaurantapp/asynctask/RestaurantDetailServiceAsyncTaskTest.java
@@ -4,7 +4,8 @@ package com.yelprestaurantapp.asynctask;
 import com.yelprestaurantapp.BuildConfig;
 import com.yelprestaurantapp.bean.RestaurantDetail;
 
-import org.junit.Assert;
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +33,9 @@ public class RestaurantDetailServiceAsyncTaskTest implements RestaurantDetailSer
         String[] params = {id};
         fixture.execute(params);
         Assert.assertNotNull(asyncResult);
+
+        Assert.assertEquals("Under The Table Restaurant", asyncResult.getName());
+        Assert.assertEquals(3, asyncResult.getCategories().size());
     }
     @Override
     public void handleAsyncResult(RestaurantDetail restaurantDetail) {
