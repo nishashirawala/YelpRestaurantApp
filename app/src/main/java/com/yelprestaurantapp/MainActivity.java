8a98cb0064c9f32783594cf161cd664d2930c59d
Nisha Shirawala <nishashirawala@gmail.com>
2016-06-23 15:07:46 -0400

    removed indexing api changes happened mistakenly

--- app/src/main/java/com/yelprestaurantapp/MainActivity.java
+++ app/src/main/java/com/yelprestaurantapp/MainActivity.java
@@ -1,27 +1,17 @@
 package com.yelprestaurantapp;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Color;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.yelprestaurantapp.adapter.RestaurantTableDataAdapter;
 import com.yelprestaurantapp.asynctask.RestaurantServiceAsyncTask;
 import com.yelprestaurantapp.bean.Restaurant;
 import com.yelprestaurantapp.listener.RestaurantDataClickListener;
-import com.yelprestaurantapp.service.RestaurantService;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -32,12 +22,6 @@ import de.codecrafters.tableview.toolkit.TableDataRowColorizers;
 
 public class MainActivity extends AppCompatActivity implements RestaurantServiceAsyncTask.ResultListener {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,16 +30,13 @@ public class MainActivity extends AppCompatActivity implements RestaurantService
         Intent intent = getIntent();
 
         String location = intent.getStringExtra("searchLocation");
-        String limit = getString(R.string.limit);
+        String limit = intent.getStringExtra("searchLimit");;
         String lat = getString(R.string.lat);
         String lon = getString(R.string.lon);
 
         String[] params = {location, limit, lat, lon};
         RestaurantServiceAsyncTask restaurantServiceAsyncTask = new RestaurantServiceAsyncTask(this);
         restaurantServiceAsyncTask.execute(params);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void updateUI(List<Restaurant> list) {
@@ -78,46 +59,6 @@ public class MainActivity extends AppCompatActivity implements RestaurantService
     @Override
     public void handleAsyncResult(List<Restaurant> restaurantList) {
         this.updateUI(restaurantList);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.yelprestaurantapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.yelprestaurantapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 
     static class RestaurantNameComparator implements Comparator<Restaurant> {
