a3013258f9efda66fba40c2d179ffa925a0e8cb2
Nisha Shirawala <nishashirawala@gmail.com>
2016-06-27 11:23:01 -0400

    Added search activity test

--- app/src/androidTest/java/com/yelprestaurantapp/SearchActivityInstrumentation.java
+++ app/src/androidTest/java/com/yelprestaurantapp/SearchActivityInstrumentation.java
@@ -13,12 +13,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public class SearchActivityInstrumentation extends ActivityInstrumentationTestCase2<SearchActivity> {
 
     @Rule
-    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(SearchActivity.class);
 
     public SearchActivityInstrumentation() {
         super(SearchActivity.class);
@@ -26,7 +31,8 @@ public class SearchActivityInstrumentation extends ActivityInstrumentationTestCa
 
     @Test
     public void testActivityLaunched() {
-        testActivityLaunched();
+        onView(withId(R.id.searchTxt)).check(matches(isDisplayed()));
+        onView(withId(R.id.searchBtn)).check(matches(isDisplayed()));
     }
 
 
