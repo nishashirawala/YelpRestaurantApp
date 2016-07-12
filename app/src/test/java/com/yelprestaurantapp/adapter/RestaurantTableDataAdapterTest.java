2e8895103ffa03f2beb3b8c0d5dcac494d1b5dc4
Nisha Shirawala <nishashirawala@gmail.com>
2016-07-12 14:55:50 -0400

    Enhance adapter test

--- app/src/test/java/com/yelprestaurantapp/adapter/RestaurantTableDataAdapterTest.java
+++ app/src/test/java/com/yelprestaurantapp/adapter/RestaurantTableDataAdapterTest.java
@@ -1,11 +1,14 @@
 package com.yelprestaurantapp.adapter;
 
-import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.common.collect.Lists;
 import com.yelprestaurantapp.BuildConfig;
 import com.yelprestaurantapp.MainActivity;
 import com.yelprestaurantapp.bean.Restaurant;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,36 +20,37 @@ import org.robolectric.annotation.Config;
 
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class RestaurantTableDataAdapterTest {
 
-    private Context mockContext;
     private MainActivity mockMainActivity;
     private RestaurantTableDataAdapter fixture;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        mockContext = mock(Context.class);
-        mockMainActivity = Robolectric.buildActivity(MainActivity.class).get();
-        fixture = new RestaurantTableDataAdapter(mockContext, mockRestaurantList());
+        mockMainActivity = Robolectric.setupActivity(MainActivity.class);
+        fixture = new RestaurantTableDataAdapter(mockMainActivity, mockRestaurantList());
     }
 
     @Test
     public void testGetCellView() throws Exception {
-        // fixture.getCellView(0, 0, null);
+        TextView restaurantName = (TextView) fixture.getCellView(0, 0, null);
+        Assert.assertEquals("A1-name", restaurantName.getText());
+        TextView restaurantAddress = (TextView) fixture.getCellView(0, 1, null);
+        Assert.assertEquals("r1-address", restaurantAddress.getText());
+        View view = fixture.getCellView(1, 1, null);
+        Assert.assertNull(view);
     }
 
 
     private List<Restaurant> mockRestaurantList() {
         List<Restaurant> list = Lists.newArrayList();
 
-        list.add(mockRestaurant("r3", "C3-name", "r3-address"));
-        list.add(mockRestaurant("r2", "B2-name", "r2-address"));
         list.add(mockRestaurant("r1", "A1-name", "r1-address"));
+        list.add(mockRestaurant("r2", "B2-name", "r2-address"));
+        list.add(mockRestaurant("r3", "C3-name", "r3-address"));
         list.add(mockRestaurant("r4", "D4-name", "r4-address"));
 
         return list;
